a8d3bbe7d3a0e4f77e4ec8b7222f9c547b7a823a
confett0 <118047961+confett0@users.noreply.github.com>
2023-01-25 18:47:21 +0100

    Add form reset after submit

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable no-unused-expressions */
-
-const myLibrary = [
-  { title: "Book1", author: "Pic", pages: 333, read: true },
-  { title: "Book2", author: "Pac", pages: 298, read: false }
-];
+const myLibrary = [];
 
 function Book(title, author, pages, read) {
 
-  this.title = title,
+    this.title = title,
     this.author = author,
     this.pages = pages,
-    this.read = read,
-    this.info = () => `${title} by ${author}, ${pages} pages`
+    this.read = read
 };
-
 
 function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read))
 }
 
+// Add default books
+
+addBookToLibrary("Alice in Wonderland", "Lewis Carroll", 198, true);
+
 const createBook = (book) => {
   const bookDiv = document.createElement("div");
   bookDiv.classList.add("book");
+
   const bookTitle = document.createElement("div");
   bookTitle.classList.add("book-title");
+
   const bookAuthor = document.createElement("div");
   bookAuthor.classList.add("book-author");
+
   const bookPages = document.createElement("div");
   bookPages.classList.add("book-pages");
+  
   document.querySelector(".book-wrap").appendChild(bookDiv);
   bookDiv.append(bookTitle, bookAuthor, bookPages);
+
   bookTitle.textContent = book.title;
   bookAuthor.textContent = book.author;
   bookPages.textContent = `${book.pages} pages`;
@@ -40,6 +41,8 @@ const renderBooks = () => {
   document.querySelector(".book-wrap").innerHTML = "";
   myLibrary.map((book) => createBook(book));
 }
+
+// Get new book data from form
 
 const form = document.getElementById("book-form");
 
@@ -54,6 +57,7 @@ form.onsubmit = (e) => {
     newBook.pages,
     newBook.read
   );
+  form.reset();
   renderBooks();
 }
 
