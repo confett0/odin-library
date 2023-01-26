b45f192e7228e93a018266f2667132761821334e
confett0 <118047961+confett0@users.noreply.github.com>
2023-01-26 20:37:23 +0100

    Create read checkbox and function to change read status

--- app.js
+++ app.js
@@ -14,14 +14,12 @@ function addBookToLibrary(title, author, pages, read) {
 
 // Add default books
 
-
-
 addBookToLibrary("Alice in Wonderland", "Lewis Carroll", 198, true);
 
 const createBook = (book) => {
   const bookDiv = document.createElement("div");
   bookDiv.classList.add("book");
-  bookDiv.setAttribute("id", myLibrary.indexOf(book));
+  bookDiv.id = myLibrary.indexOf(book);
 
   const bookTitle = document.createElement("div");
   bookTitle.classList.add("book-title");
@@ -31,17 +29,40 @@ const createBook = (book) => {
 
   const bookPages = document.createElement("div");
   bookPages.classList.add("book-pages");
+
+  const readCheckbox = document.createElement("input");
+  readCheckbox.type = "checkbox";
+  readCheckbox.id = "read-checkbox";
+  readCheckbox.name = "read";
+
+  const label = document.createElement("label");
+  label.htmlFor = "read-checkbox";
+  label.textContent = "Read?"
   
   const delButton = document.createElement("div");
   delButton.classList.add("del-button");
   
   document.querySelector(".book-wrap").appendChild(bookDiv);
-  bookDiv.append(bookTitle, bookAuthor, bookPages, delButton);
+  bookDiv.append(bookTitle, bookAuthor, bookPages, label, readCheckbox, delButton);
 
   bookTitle.textContent = book.title;
   bookAuthor.textContent = book.author;
   bookPages.textContent = `${book.pages} pages`;
   delButton.textContent = "X";
+
+  if (book.read === true) {
+    readCheckbox.checked = true;
+  } else {
+    readCheckbox.checked = false;
+  }
+
+  readCheckbox.addEventListener("change", () => {
+    if (readCheckbox.checked) {
+      book.read = true;
+    } else {
+      book.read = false;
+    }
+  })
 
   delButton.addEventListener("click", () => {
     myLibrary.splice(myLibrary.indexOf(book),1);
@@ -54,7 +75,6 @@ const renderBooks = () => {
   myLibrary.map((book) => createBook(book));
 }
 
-
 // Get new book data from form
 
 const form = document.getElementById("book-form");
@@ -63,7 +83,15 @@ form.onsubmit = (e) => {
   e.preventDefault();
   const formData = new FormData(form);
   const newBook = {};
+  const checkbox = formData.get("read");
+  if (!checkbox) {
+    formData.append("read", false);
+  } else {
+    formData.append("read", true);
+  }
+
   formData.forEach((value, key) => (newBook[key] = value));
+  
   addBookToLibrary(
     newBook.title,
     newBook.author,
